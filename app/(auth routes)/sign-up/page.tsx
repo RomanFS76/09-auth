9660bca9911836e284a464437ad21e6a7b76c231
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-02-01 12:05:30 +0200

    refactor: improve signup submit error handling and redirect logic feat(auth): add register API route with cookie parsing and logging

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -1,11 +1,40 @@
+'use client';
+
 import css from './SignUpPage.module.css';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { register, RegisterRequest } from '@/lib/clientApi';
+import { ApiError } from '@/app/api/api';
 
 const SignUpPage = () => {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues = Object.fromEntries(formData) as RegisterRequest;
+
+      const res = await register(formValues);
+
+      if (res) {
+        router.push('/profile');
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (error) {
+      setError(
+        (error as ApiError).response?.data?.error ??
+          (error as ApiError).message ??
+          'Oops... some error'
+      );
+    }
+  };
+
   return (
     <>
       <main className={css.mainContent}>
         <h1 className={css.formTitle}>Sign up</h1>
-        <form className={css.form}>
+        <form className={css.form} action={handleSubmit}>
           <div className={css.formGroup}>
             <label htmlFor="email">Email</label>
             <input
@@ -16,7 +45,6 @@ const SignUpPage = () => {
               required
             />
           </div>
-
           <div className={css.formGroup}>
             <label htmlFor="password">Password</label>
             <input
@@ -27,14 +55,12 @@ const SignUpPage = () => {
               required
             />
           </div>
-
           <div className={css.actions}>
             <button type="submit" className={css.submitButton}>
               Register
             </button>
           </div>
-
-          <p className={css.error}>Error</p>
+          <p className={css.error}>{error}</p>
         </form>
       </main>
     </>
--- lib/clientApi.ts
+++ lib/clientApi.ts
@@ -1,8 +1,21 @@
+import { User } from '@/types/user';
+import { nextServer } from './api';
+
+export type RegisterRequest = {
+  email: string;
+  password: string;
+};
+
+export const register = async (data: RegisterRequest) => {
+  const res = await nextServer.post<User>('/auth/register', data);
+  return res.data;
+};
+
 // fetchNotes
 // fetchNoteById
 // createNote
 // deleteNote
-// register
+// register+++
 // login
 // logout
 // checkSession
