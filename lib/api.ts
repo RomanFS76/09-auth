6cdc87f7b6e8ce3d7ecc310a6db4e8d7e762d6c7
RomanFS76 <korvy85@gmail.com>
2026-02-04 15:26:19 +0300

    feat(api): enable credentials for next server axios client

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-// import type { Note } from '../types/note';
 
 export const nextServer  = axios.create({
   baseURL: 'http://localhost:3000/api',
+  withCredentials: true,
   headers: {
     accept: 'application/json',
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
