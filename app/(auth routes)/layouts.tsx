2fb15dfb79072adf84632dda8499b1aaf18a6727
RomanFS76 <korvy85@gmail.com>
2026-02-06 19:13:45 +0300

    fix(layout): correct children props typing in PublicLayout

--- app/(auth routes)/layouts.tsx
+++ app/(auth routes)/layouts.tsx
@@ -1,22 +1,7 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-
 type Props = {
   children: React.ReactNode;
 };
 
-
-export default function PublicLayout({ children }: { children: Props }) {
-  const [loading, setLoading] = useState(true);
-
-  const router = useRouter();
-
-
-  useEffect(() => {
-    router.refresh();
-    setLoading(false);
-  }, [router]);
-
-  return <>{loading ? <div>Loading...</div> : children}</>;
+export default function PublicLayout({ children }: Props) {
+  return <>{children}</>;
 }
